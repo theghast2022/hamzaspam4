2f31ca42810b8139ce588e2611434c7d07b3a478
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-11-10 20:29:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "9"
+var prefix = "9";
 
 client.on('ready', () => {
   console.log('Logged in as ${client.user.tag}!');
@@ -11,7 +11,7 @@ client.on('ready', async() => {
 var server = "510887418874953737"; // ايدي السررفر
 var channel = "510887418874953739";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , ')
+    client.guilds.get(server).channels.get(channel).send('The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast, The_Ghast, The_Ghast , The_Ghast , The_Ghast , The_Ghast , The_Ghast , ')
     },305);
 })
 
